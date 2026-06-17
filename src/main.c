dd5148658f6f9bd3c415aa46abbc6b53a6c4b386
pedrocm15 <pedroaugusto.melocm@gmail.com>
2026-06-17 15:51:51 -0300

    colocando mais bolinhas

--- src/main.c
+++ src/main.c
@@ -23,18 +23,19 @@ Camera2D camera = {0};
 
 InitCamera(&camera, &player);
 
-Ball balls[4];
+Ball balls[7];
 
 InitBall(&balls[0], 156, 1481, 200, 0); 
 InitBall(&balls[1], 1540, 1160, 100, 120); 
 InitBall(&balls[2], 1187, 109, -200, 0); 
 InitBall(&balls[3], 131, 227, 120, 40); 
-
-
+InitBall(&balls[4], 801, 864, 150, 0);
+InitBall(&balls[5], 354, 863, 0, 220);
+InitBall(&balls[6], 915, 290, 100, 0);
 
     while (!WindowShouldClose()) {
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 7; i++) {
             UpdateBall(&balls[i]);
         }
 
@@ -53,7 +54,7 @@ InitBall(&balls[3], 131, 227, 120, 40);
             
             desenhar_mapa();
 
-              for (int i = 0; i < 4; i++) {
+              for (int i = 0; i < 7; i++) {
             DrawBall(&balls[i]);
 
 
@@ -62,9 +63,12 @@ InitBall(&balls[3], 131, 227, 120, 40);
 
             DrawPlayer(&player);
             
+            
+
              EndMode2D();
              
-             
+             DrawText(TextFormat("X: %.0f", player.x), 20, 20, 20, RED);
+DrawText(TextFormat("Y: %.0f", player.y), 20, 45, 20, RED);
             
             
             DrawFPS(10, 10);
